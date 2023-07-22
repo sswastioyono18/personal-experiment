52310769d8df984ee3025e94c302cab55b3297a6
sswastioyono18 <sactio@kitabisa.com>
2023-07-23 00:09:40 +0700

    feat: added prometheus

--- go-k8s-example/main.go
+++ go-k8s-example/main.go
@@ -1,58 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rs/zerolog/log"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	log.Info().Msg("Hello world info")
-	log.Warn().Err(errors.New("testaja")).Msg("test aja kok")
-
-	log.Warn().Err(errors.New("testaja")).Msg("test aja dong")
-	loc, _ := time.LoadLocation("Asia/Jakarta")
-	t := time.Now().In(loc)
-	hour := t.Format("15:04")
-	type Test struct {
-		Test string `json:"test"`
-	}
-
-	test123 := TestLife{}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(test123)
-	fmt.Println(hour)
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
 }
 
-type TestLife struct {
-	TestAja string `json:"test_aja,omitempty"`
-}
-
-func check(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Health check</h1>")
-}
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
+	recordMetrics()
 
-	//compressor := middleware.NewCompressor(5, "/*")
-	//compressor.SetEncoder("br", func(w io.Writer, level int) io.Writer {
-	//	params := enc.NewBrotliParams()
-	//	params.SetQuality(level)
-	//	return enc.NewBrotliWriter(params, w)
-	//})
-	//r.Use(compressor.Handler)
-	r.Use(middleware.Compress(5))
-	r.Get("/test", index)
-	r.Get("/health_check", check)
-
-	fmt.Println("Server starting...")
-	http.ListenAndServe(":8080", r)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
